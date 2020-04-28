0eab4cd89a9c2598fb6c8024078dc7a2918a6352
Bhavya <bhavyachowdary.manam@gmail.com>
2020-04-28 22:34:36 +0530

    Updates

---
+++ precache-manifest.75319a35085858ee0d49160665f5a057.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fbd7701c1844f93137f5c167946af66",
+    "url": "/profile1/index.html"
+  },
+  {
+    "revision": "b27890fdfb8632946ffc",
+    "url": "/profile1/static/css/main.0fc39633.chunk.css"
+  },
+  {
+    "revision": "5cef74052626a436d4a0",
+    "url": "/profile1/static/js/2.f9fe3acf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/profile1/static/js/2.f9fe3acf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b27890fdfb8632946ffc",
+    "url": "/profile1/static/js/main.b3833b43.chunk.js"
+  },
+  {
+    "revision": "ef84e7081b6ebed31ee8",
+    "url": "/profile1/static/js/runtime-main.03e0e521.js"
+  }
+]);
